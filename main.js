10729f5fa4d7414bd1d4295033ff4b1b64dcedbd
Gustavo Inácio <gustavo.user0605@gmail.com>
2025-03-14 14:14:31 -0300

    14h14

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-const prompt=require("prompt-sync")();
+const prompt = require("prompt-sync")();
 
 var i, j, lin, col;
-var jogadas=0;
-var jogador='o';
-var vencedor=' ';
+var jogadas = 0;
+var jogador = 'o';
+var vencedor = ' ';
 var fimJogo = false;
-var tab = [3];
-//a
-while ((jogadas < 9) && (fimJogo==false)){
+var tab = [];
+
+for (i = 0; i < 3; i++) {
+    tab[i] = []; 
+    for (j = 0; j < 3; j++) {
+        tab[i][j] = ' '; 
+    }
+}
+
+while ((jogadas < 9) && (fimJogo == false)) {
     //b
-    lin=parseInt(prompt(`${jogador} em [linha]: `));
-    col=parseInt(prompt(`${jogador} em [coluna]: `));
+    lin = parseInt(prompt(`${jogador} em [linha]: `));
+    col = parseInt(prompt(`${jogador} em [coluna]: `));
     //c
 }
+
 //d
-if(vencedor=' '){
-    console.log("emapte\n");
-}else{
-    console.log(`${vencedor} venceu`)
+if (vencedor == ' ') {
+    console.log("empate\n");
+} else {
+    console.log(`${vencedor} venceu`);
 }
