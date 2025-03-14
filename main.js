45a7bcb1903523b1130a180efb1d6128a2288c29
Gustavo Inácio <gustavo.user0605@gmail.com>
2025-03-14 15:17:26 -0300

    letra-d

--- main.js
+++ main.js
@@ -5,75 +5,80 @@ var jogadas = 0;
 var jogador = 'o';
 var vencedor = ' ';
 var fimJogo = false;
-var tab = [3];
+var tab = [];
 var count = 0;
 
 for (i = 0; i < 3; i++) {
-    tab[i] = []; 
+    tab[i] = [];
     for (j = 0; j < 3; j++) {
-        tab[i][j] = ' '; 
+        tab[i][j] = ' ';
     }
 }
 
 while ((jogadas < 9) && (fimJogo == false)) {
-    for(i = 0; i < 3; i++){
-        console.log(`[${tab[i]}]`)
+    for (i = 0; i < 3; i++) {
+        console.log(`[${tab[i]}]`);
     }
 
     lin = parseInt(prompt(`${jogador} em [linha]: `));
     col = parseInt(prompt(`${jogador} em [coluna]: `));
-    if(lin || col > 2){
+
+    if (lin < 0 || lin > 2 || col < 0 || col > 2) {
         console.log("Jogada além dos limites");
+        continue;
     }
-    if(tab[lin][col] != ' '){
+    if (tab[lin][col] != ' ') {
         console.log("Coordenada cheia!");
+        continue;
+    } else {
+        tab[lin][col] = jogador;
+        jogadas++;
     }
-    else{
-        tab[lin][col] = 'o';
-    }
-        // primeira situação (vertical)
-        for (i = 1; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                if (tab[i][j] != ' ' && tab[i][j] == tab[i - 1][j]) {
-                    count++;
-                }
-            }
-        }
 
-        // segunda situação (horizontal)
-        for (i = 0; i < 3; i++) {
-            for (j = 1; j < 3; j++) {
-                if (tab[i][j - 1] != ' ' && tab[i][j - 1] == tab[i][j]) {
-                    count++;
-                }
-            }
-        }
+    count = 0; 
 
-        // terceira situação (diagonais)
-        if (tab[0][0] != ' ' && tab[0][0] == tab[1][1] && tab[1][1] == tab[2][2]) {
+    //primeira situação (vertical)
+    for (j = 0; j < 3; j++) {
+        if (tab[0][j] != ' ' && tab[0][j] == tab[1][j] && tab[1][j] == tab[2][j]) {
             count = 3;
         }
+    }
 
-        if (tab[0][2] != ' ' && tab[0][2] == tab[1][1] && tab[1][1] == tab[2][0]) {
+    //segunda situação (horizontal)
+    for (i = 0; i < 3; i++) {
+        if (tab[i][0] != ' ' && tab[i][0] == tab[i][1] && tab[i][1] == tab[i][2]) {
             count = 3;
         }
-        //count >= vencedor
-        if (count >= 3) {
-            vencedor = jogador;
-            fimJogo = true;
-        } else {
-            if(jogador == 'o'){
-                jogador = 'x';
-            }
-            else{
-                jogador = 'o';
-            }
+    }
+
+    //terceira situação (diagonais)
+    if (tab[0][0] != ' ' && tab[0][0] == tab[1][1] && tab[1][1] == tab[2][2]) {
+        count = 3;
+    }
+
+    if (tab[0][2] != ' ' && tab[0][2] == tab[1][1] && tab[1][1] == tab[2][0]) {
+        count = 3;
+    }
+
+    //count>=3
+    if (count >= 3) {
+        vencedor = jogador;
+        fimJogo = true;
+    } else {
+        if(jogador == 'o'){
+            jogador = 'x';
+        }else{
+            jogador = 'o';
         }
+    }
 }
 
-//d
+for (i = 0; i < 3; i++) {
+    console.log(`[${tab[i]}]`);
+}
+
 if (vencedor == ' ') {
-    console.log("empate\n");
+    console.log("Empate\n");
 } else {
     console.log(`${vencedor} venceu`);
 }
